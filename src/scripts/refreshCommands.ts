7914693a0237a4b4a74fe3de32e9dbdbdcc494cf
Cryptic42069 <69015627+Cryptic42069@users.noreply.github.com>
2025-01-10 19:25:00 -0500

    added the "-d/--de-reg" param so that you can deregister commands

--- src/scripts/refreshCommands.ts
+++ src/scripts/refreshCommands.ts
@@ -4,13 +4,14 @@ import { parseArgs } from "jsr:@std/cli/parse-args";
 import CommandHandler from "../lib/handlers/file-handlers/CommandHandler.ts";
 
 const flags = parseArgs(Deno.args, {
-    boolean: ["g", "global"],
+    boolean: ["g", "global", "d", "de-reg"],
 });
 
 function refreshCommands(): Promise<void> {
     return new Promise(async (resolve) => {
         let ch = CommandHandler.getInstance
-        await ch.loadSlashCommandFolder("src/commands")
+        if (!flags["de-reg"] && !flags.d)
+            await ch.loadSlashCommandFolder("src/commands")
         await ch.refreshSlashCommandRegistry(Deno.env.get("CLIENT_LOGIN_TOKEN"), flags.g || flags.global)
         resolve()
     })
