34d6fb3038f90d5fea598097b5a170cec7197a00
Cryptic <69015627+Cryptic42069@users.noreply.github.com>
2025-05-05 20:27:07 -0400

    clear up imports, update data files on startup, and cron job update data files at midnight

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,11 @@
-import { Client, GatewayIntentBits, Partials } from "discord.js";
+import { Client, GatewayIntentBits  } from "discord.js";
 import "jsr:@std/dotenv/load";
 import Debug from "./lib/util/Debug.ts";
 import path from "node:path"
 import EventHandler from "./lib/handlers/file-handlers/EventHandler.ts";
+import EpisodeScraper from "./lib/scrapers/episodeScraper.ts";
+import ChapterScraper from "./lib/scrapers/chapterScraper.ts";
+import { CronJob } from 'cron';
 
 export const client = new Client({ intents: [GatewayIntentBits.GuildExpressions], partials: [] });
 
@@ -14,13 +17,28 @@ async function start() {
 
     Debug.logStartup("Starting client...", loggerID)
 
+    await EpisodeScraper.updateDataFile()
+    await ChapterScraper.updateDataFile()
+    
+    CronJob.from({
+        cronTime: "0 0 0 * * *", 
+        onTick: async () => {
+            try {
+                await EpisodeScraper.updateDataFile();
+                await ChapterScraper.updateDataFile();
+            } catch (error) {
+                Debug.logError(`Error during cron job execution: ${error}`, loggerID);
+            }
+        }, 
+        start: true, 
+        timeZone: "system"
+    })
+
     let eh = EventHandler.getInstance
     eh.cacheClient(client)
     await eh.loadEventFolder("src/events")
 
     await client.login(Deno.env.get("CLIENT_LOGIN_TOKEN"));
-
-    client.emojis.cache.forEach((emoji) => console.log(emoji))
 }
 start()
 
